116754050d52b1acc98c5095eee3c179e17ab8e8
Mohamed Elkmeshi <M.Elkmeshi@prestoeat.com>
2026-02-15 13:28:57 +0200

    wip

---
+++ update.sh
@@ -0,0 +1,8 @@
+git pull
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan optimize:clear
+npm run build
+pm2 restart telegram-shop-api --update-env
