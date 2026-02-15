016e0c27d0eab16edbdef8761739b99f37dab38c
Mohamed Elkmeshi <M.Elkmeshi@prestoeat.com>
2026-02-15 02:05:28 +0200

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class,
+            VozolSeeder::class,
         ]);
     }
 }
